c175ae6ef5725c5d8caca28c8804447d183c0f00
Benthan <bxvu@ucsc.edu>
2023-01-16 17:28:12 -0800

    functionality of set and get done, passes given tests, still need to check for other cases

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -3,75 +3,75 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-
 #include <stdio.h>
+
 //#include "memory_functions.h"
-#define MAX_LENGTH = 128;
 
 int main() {
     int infile;
-    //char buffer[1]; //(MAX_LENGTH * 8)];
     char one;
-    char token[1024] = "";
+    char buffer[1024] = "";
+    char *command;
+    char *filename;
+
     while (read(STDIN_FILENO, &one, sizeof(one)) > 0) {
         if (one == '\n') {
             break;
         }
-        strncat(token, &one, 1);
-        //printf("%s\n", token);
+        strncat(buffer, &one, 1);
     }
-    //printf("%lu\n", strlen(token));
-    //printf("%lu\n", sizeof(token));
-    char *tok;
-    //printf("%s\n", token);//set hello.txt\nhilo
-    tok = strtok(token, " ");
-    //printf("%s\n", token);
-    //printf("%s\n", buffer);
-    if (!strcmp(tok, "set")) {
-        tok = strtok(NULL, " "); //need to make it look for \n
-        //printf("%lu\n", strlen(tok));
-        //printf("%lu\n", sizeof(tok));
-        //write(STDOUT_FILENO, tok, sizeof(tok));
-        infile = open(tok, O_WRONLY | O_CREAT);
-        if (infile > 0) {
-            ftruncate(infile, 0);
-        }
-        token[0] = '\0';
-        while (read(STDIN_FILENO, &one, sizeof(one)) > 0){
-            strncat(token, &one, 1);
-            /* if (one == '\n') {
-                break;
-            }*/
-            //printf("%s\n", token);
-        }
-        write(infile, token, strlen(token));
-        //char input[strlen(token)];
-        //strcpy(input, token);
-        //printf("%lu\n", strlen(input));
-        //printf("%lu\n", sizeof(input));
-        close(infile);
-        write(STDOUT_FILENO, "OK\n", sizeof("OK\n"));
-        return 0;
-    } 
-    else if (!strcmp(token, "get")) {
-        /* token = strtok(NULL, "\n");
-        if (!token) {
-            write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command\n"));
-            return 1;
-        }
-        infile = open(token, O_RDONLY);
-        if (infile == -1 || strtok(NULL, "") != NULL) {
-            write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command\n"));
-            return 1;
-        }
-        read(infile, buffer, sizeof(buffer));
-        write(STDOUT_FILENO, buffer, sizeof(buffer));
-        return 0;*/
-    } 
-    else {
-        write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command\n"));
-        //printf("Invalid Command\n");
+    if (strlen(buffer) == 0) {
         return 1;
     }
-    //free(buffer);
+    command = strdup(strtok(buffer, " "));
+    filename = strdup(strtok(NULL, " "));
+    if (strlen(filename) == 0 || strlen(filename) > PATH_MAX || strtok(NULL, " ") != NULL) {
+        write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
+        return 1;
+    }
+    buffer[0] = '\0';
+    if (!strcmp(command, "set")) {
+        infile = open(filename, O_WRONLY | O_CREAT);
+        if (infile > 0) {
+            ftruncate(infile, 0);
+        } else if (infile == -1) {
+            write(STDERR_FILENO, "Operation Failed\n", sizeof("Operation Failed\n"));
+            return 1;
+        }
+        while (read(STDIN_FILENO, &one, sizeof(one)) > 0){
+            strncat(buffer, &one, 1);
+        }
+        if (strlen(buffer) == 0) {
+            ftruncate(infile, 0);
+        } else {
+            write(infile, buffer, strlen(buffer));
+        }
+        close(infile);
+        write(STDOUT_FILENO, "OK\n", sizeof("OK"));
+        return 0;
+    } 
+    else if (!strcmp(command, "get")) {
+        infile = open(filename, O_RDONLY);
+        if (infile == -1){
+            write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
+            return 1;
+        }
+        while (read(infile, &one, sizeof(one)) > 0) {
+            strncat(buffer, &one, 1);
+        }
+        write(STDOUT_FILENO, buffer, strlen(buffer));
+        return 0;
+    } 
+    else {
+        write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
+        return 1;
+    }
 }
+
+        /* tok = strtok(NULL, " "); //need to make it look for \n
+        printf("%lu\n", strlen(tok));
+        printf("%lu\n", sizeof(tok));
+        write(STDOUT_FILENO, token, sizeof(token));*/
+        //printf("%lu\n", strlen(input));
+//printf("%lu\n", sizeof(input));
+//printf("%s\n", buffer);*/
