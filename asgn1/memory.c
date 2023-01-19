35f27578f3a7b04880c759252d9e46f2ee483960
Benthan <bxvu@ucsc.edu>
2023-01-19 14:25:40 -0800

    changed program to match with example given in class since it looks better, still need to pass some tests

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -12,47 +12,210 @@ int nullTerminatorCheck(char *f) {
         if (i + 1 < (int) strlen(f) && f[i] == '\\' && f[i + 1] == '0') {
             return 1;
         }
+        if (f[i] == '/') {
+            return 1;
+        }
     }
     return 0;
 }
 
-int cleanupReturn(int returnCode, char *fn, char *cmd) {
+int cleanupReturn(int returnCode, char *fn, char *cmd, int infile) {
     if (fn)
         free(fn);
     if (cmd)
         free(cmd);
+    if (infile > 0)
+        close(infile);
     return returnCode;
 }
 
-/* void updateCommandAndFilename(char *command, char *filename, char *buffer) {
-    for (int i = 0; i < (int) strlen(buffer); i++) {
-        if (buffer[i] == ' ' && !command) {
-            command = strdup(reade);
-            reade[0] = '\0';
-            continue;
-            //if (i + 1 < (int) strlen(buffer))
-            //i++;
-        } else if ((buffer[i] == ' ' || buffer[i] == '\n') && command && !filename) {
-            filename = strdup(reade);
-            reade[0] = '\0';
-
-            //printf("fn: %s\n", filename);
-            //if (i+1 < (int)strlen(buffer)) i++;
-        }
-        //printf("re: %s\n", reade);
-        strncat(reade, &buffer[i], 1);
-    }
+/* void getCommand(char *command, char *buffer, char *token) {
+    token = strtok(buffer, " ");
+    command = strdup(token);
+}
+void getFilename(char *filename, char *buffer, char *token) {
+    const char s[2] = "\n";
+    token = strtok(NULL, s);
+    filename = strdup(buffer);
 }*/
+
+int writeCommand(int infile, char *buffer, int bytes_written, int *bytes_read) {
+    do {
+        int bytes = write(infile, &buffer[bytes_written], *bytes_read - bytes_written);
+        if (bytes < 0) {
+            return 1;
+        }
+        bytes_written += bytes;
+    } while (bytes_written < *bytes_read);
+    return 0;
+}
+
+int checkEquality(char *buffer, char *command) {
+    for (int i = 0; i < (int) strlen(command); i++) {
+        if (buffer[i] != command[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int main() {
     int infile = -2;
-    int trunc = 0;
-    char one;
+    //int trunc = 0;
+    //char one;
     char buffer[8192] = "";
-    //char *token;
+    char *token = NULL;
     //char reade[8192] = "";
     char *command = NULL;
     char *filename = NULL;
+    char buffy[8192];
+    int bytes_read = 0;
+
+    do {
+        bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer));
+        if (bytes_read < 0) {
+            write(STDERR_FILENO, "Operation Failed\n", sizeof("Operation Failed"));
+            return cleanupReturn(1, filename, command, infile);
+        } else if (bytes_read > 0) {
+            if (infile != -2 && command && filename) {
+                if (!strcmp(command, "set")) {
+                    //int bytes_written = 0;
+                    if (writeCommand(infile, buffer, 0, &bytes_read)) {
+                        write(STDERR_FILENO, "Operation Failed\n", sizeof("Operation Failed"));
+                        return cleanupReturn(1, filename, command, infile);
+                    }
+                }
+                continue;
+            }
+            if (!command) {
+                //getCommand(command, buffer, token);
+                strcpy(buffy, buffer);
+                token = strtok(buffy, " ");
+                command = strdup(token);
+            }
+            if (!filename && command) {
+                //getFilename(filename, buffer + 1, token);
+                const char n[3] = " \n";
+                //const char s[1] = " ";
+                //printf("hi|%d", bytes_read);
+                token = strtok(NULL, n);
+                if (token == NULL || strlen(token) == 0 || strlen(token) > PATH_MAX
+                    || nullTerminatorCheck(token)) {
+                    write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
+                    return cleanupReturn(1, filename, command, infile);
+                }
+                filename = strdup(token);
+                /* printf("filename: %s|\n", filename);
+                printf("command: %s|\n", command);
+                printf("buffer: %s|\n", buffer);
+                printf("token: %s|\n", token);
+                printf("-n: %lu|\n", strlen(buffer));
+                printf("c: %c|\n", buffer[strlen(buffer) - 1]);*/
+                if (!strcmp(command, "set")) {
+                    if (buffer[strlen(buffer) - 1] == '\n'
+                        || buffer[strlen(command) + strlen(filename) + 1] == '\n') {
+                        infile = open(filename, O_TRUNC | O_WRONLY | O_CREAT);
+                        //printf("hi|%d", bytes_read);
+                        if (infile == -1) {
+                            write(STDERR_FILENO, "Operation Failed\n", sizeof("Operation Failed"));
+                            return cleanupReturn(1, filename, command, infile);
+                        }
+                        /* printf("buffy: %s|\n", buffy);
+                        printf("buffer: %s|\n", buffer);
+                        printf("token: %s|\n", token);*/
+                        //buffer[strlen(buffer) - 1] = ' ';
+                        //memmove(buffer, &buffer[strlen(filename)+1],strlen(buffer) - strlen(filename)-1);
+                        //memcpy(buffer, &buffer[strlen(command) + strlen(filename) + 1], strlen(buffer));
+                        //printf("buffer: %s|\n", buffer);
+                        if (writeCommand(infile, buffer, strlen(command) + strlen(filename) + 2,
+                                &bytes_read)) {
+                            write(STDERR_FILENO, "Operation Failed\n", sizeof("Operation Failed"));
+                            return cleanupReturn(1, filename, command, infile);
+                        }
+                        /* printf("buffer: %s\n", buffer);
+                        if (writeCommand(infile, buffer, strlen(command) + strlen(filename) + 1, &bytes_read)) {
+                            write(STDERR_FILENO, "Operation Failed\n", sizeof("Operation Failed"));
+                            return cleanupReturn(1, filename, command, infile);
+                        }
+                        write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
+                        return cleanupReturn(1, filename, command, infile);*/
+                    }
+                } else if (!strcmp(command, "get")) {
+                    if (strlen(buffer) != strlen(filename) + strlen(command) + 2) {
+                        infile = -3; //extra spaces aafter filename
+                    }
+                }
+            }
+            /* printf("filename: %s\n", filename);
+            printf("command: %s\n", command);
+            printf("buffer: %s\n", buffer);*/
+        }
+    } while (bytes_read > 0);
+    if (!command && !filename && strlen(buffer) == 0) {
+        write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
+        return 1;
+    }
+    if (infile != -3 && command && !strcmp(command, "get")) {
+        infile = open(filename, O_RDONLY);
+        /* printf(
+            "%lu, %lu, %lu, %lu", strlen(buffer), strlen(command), strlen(filename), strlen(token));
+        printf("filename: %s|\n", filename);
+        printf("command: %s|\n", command);
+        printf("buffer: %s|\n", buffer);
+        printf("token: %s|\n", token);
+        printf("-n: %lu|\n", strlen(buffer));
+        printf("c: %c|\n", buffer[strlen(buffer)-1]);*/
+        if (infile == -1 || buffer[strlen(buffer) - 1] != '\n' || checkEquality(buffer, command)) {
+            write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
+            return cleanupReturn(1, filename, command, infile);
+        }
+        do {
+            bytes_read = read(infile, buffer, sizeof(buffer));
+            if (bytes_read < 0) {
+                write(STDERR_FILENO, "Operation Failed\n", sizeof("Operation Failed"));
+                return cleanupReturn(1, filename, command, infile);
+            } else if (bytes_read > 0) {
+                //int bytes_written = 0;
+                if (writeCommand(STDOUT_FILENO, buffer, 0, &bytes_read)) {
+                    write(STDERR_FILENO, "Operation Failed\n", sizeof("Operation Failed"));
+                    return cleanupReturn(1, filename, command, infile);
+                }
+            }
+        } while (bytes_read > 0);
+        close(infile);
+        return cleanupReturn(0, filename, command, infile);
+    } else if (infile != -2 && !strcmp(command, "set")) {
+        //close(infile);
+        write(STDOUT_FILENO, "OK\n", sizeof("OK"));
+        return cleanupReturn(0, filename, command, infile);
+    }
+    //if (infile != -2) {
+    //    close(infile);
+    //}
+    write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
+    return cleanupReturn(1, filename, command, infile);
+}
+
+/* do {
+        bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer);
+        if (bytes_read < 0) {
+            write(STDERR_FILENO, "Operation Failed\n", sizeof("Operation Failed"));
+            return cleanupReturn(1, filename, command);
+        }
+        else if (bytes_read > 0) {
+            int bytes_written = 0;
+            do {
+                int bytes
+                    = write(STDOUT_FILENO, buffer + bytes_written, bytes_read - bytes_written);
+                if (bytes <= 0) {
+                    write(STDERR_FILENO, "Operation Failed\n", sizeof("Operation Failed"));
+                    return cleanupReturn(1, filename, command);
+                }
+                bytes_written += bytes;
+            } while (bytes_written < bytes_read);
+        }
+    } while (bytes_read > 0);
+
 
     while (read(STDIN_FILENO, &one, sizeof(one)) > 0) {
         //if (sizeof(buffer) == strlen(buffer)){ //&& !command && !filename) {
@@ -98,14 +261,14 @@ int main() {
         }
         strncat(buffer, &one, 1);
     }
-    /* printf("filename: %s", filename);
+    / printf("filename: %s", filename);
     printf("command: %s", command);
-    printf("buffer: %s", buffer);*/
+    printf("buffer: %s", buffer);/
     if (!command && !filename && strlen(buffer) == 0) {
         write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
         return 1;
     }
-    /* for (int i = 0; i < (int) strlen(buffer); i++) {
+    / for (int i = 0; i < (int) strlen(buffer); i++) {
         if (buffer[i] == ' ' && !command) {
             command = strdup(reade);
             reade[0] = '\0';
@@ -115,7 +278,7 @@ int main() {
             reade[0] = '\0';
         }
         strncat(reade, &buffer[i], 1);
-    }*/
+    }/
     if (!command) {
         write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
         return cleanupReturn(1, filename, command);
@@ -177,44 +340,5 @@ int main() {
         write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
         return cleanupReturn(1, filename, command);
     }
-    /* printf("filename: %s", filename);
-    printf("command: %s", command);
-    printf("token: %s", token);*/
-    /* buffer[0] = '\0';
-    if (!strcmp(command, "set")) {
-        if (reade[0] != '\n') {
-            write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
-            return cleanupReturn(1, filename, command);
-        }
-        memmove(reade, reade + 1, strlen(reade));
-        infile = open(filename, O_TRUNC | O_WRONLY | O_CREAT);
-        if (infile == -1) {
-            write(STDERR_FILENO, "Operation Failed\n", sizeof("Operation Failed"));
-            return cleanupReturn(1, filename, command);
-        }
-        write(infile, reade, strlen(reade));
-        close(infile);
-        write(STDOUT_FILENO, "OK\n", sizeof("OK"));
-        return cleanupReturn(0, filename, command);
-    } else if (!strcmp(command, "get")) {
-        //if ((reade[0] != '\n' && strlen(reade) > 0) || (reade[0] == '\n' && strlen(reade) > 1)) { //if both "get foo.txt" and "get foo.txt\n" allowed
-        if (reade[0] != '\n'
-            || (reade[0] == '\n' && strlen(reade) > 1)) { //if only "get foo.txt" allowed
-            write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
-            return cleanupReturn(1, filename, command);
-        }
-        infile = open(filename, O_RDONLY);
-        if (infile == -1) {
-            write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
-            return cleanupReturn(1, filename, command);
-        }
-        while (read(infile, &one, sizeof(one)) > 0) {
-            strncat(buffer, &one, 1);
-        }
-        write(STDOUT_FILENO, buffer, strlen(buffer));
-        return cleanupReturn(0, filename, command);
-    } else {
-        write(STDERR_FILENO, "Invalid Command\n", sizeof("Invalid Command"));
-        return cleanupReturn(1, filename, command);
-    }*/
-}
+    
+}*/
